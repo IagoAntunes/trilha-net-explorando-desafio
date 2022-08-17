f1db1b0f9ea44604fbbd46173cd7095b8b08cef5
IagoAntunes <iagoantunes.f@gmail.com>
2022-08-17 10:23:17 -0300

    Solução

--- Program.cs
+++ Program.cs
@@ -11,6 +11,8 @@ Pessoa p2 = new Pessoa(nome: "Hóspede 2");
 
 hospedes.Add(p1);
 hospedes.Add(p2);
+hospedes.Add(p2);
+hospedes.Add(p2);
 
 // Cria a suíte
 Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
